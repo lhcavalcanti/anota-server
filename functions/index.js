befa98840c60e9c8e337184fb6dc15a73032aee2
chcp <chcp@cin.ufpe.br>
2018-05-28 20:04:09 -0300

    Remove console.log

--- functions/index.js
+++ functions/index.js
@@ -30,67 +30,67 @@ exports.addList = functions.https.onRequest((req, res) => {
 
     requestList(link, uid, Date.now(), res);
 
-});
+  });
 
 function requestList(link, uid, date, res) {
-    request(link, (error, response, html) => {
-        if (!error) {
-            let doc = cheerio.load(html);
+  request(link, (error, response, html) => {
+    if (!error) {
+      let doc = cheerio.load(html);
 
-            var prodCode = [];
-            var prod = {};
-            doc('xProd').each(function (i, element) {
-                prodCode[i] = doc(this).text();
-                prod[prodCode[i]] = {
+      var prodCode = [];
+      var prod = {};
+      doc('xProd').each(function (i, element) {
+        prodCode[i] = doc(this).text();
+        prod[prodCode[i]] = {
                     // name: doc(this).text()
-                };
-            });
-            doc('cProd').each(function (i, element) {
-                (prod[prodCode[i]])["code"] = doc(this).text();
-            });
-            doc('uCom').each(function (i, element) {
-                (prod[prodCode[i]])["un"] = doc(this).text();
-            });
-            doc('qCom').each(function (i, element) {
-                (prod[prodCode[i]])["qtd"] = doc(this).text();
-            });
-            doc('vUnCom').each(function (i, element) {
-                (prod[prodCode[i]])["priceUnit"] = doc(this).text();
-            });
+                  };
+                });
+      doc('cProd').each(function (i, element) {
+        (prod[prodCode[i]])["code"] = doc(this).text();
+      });
+      doc('uCom').each(function (i, element) {
+        (prod[prodCode[i]])["un"] = doc(this).text();
+      });
+      doc('qCom').each(function (i, element) {
+        (prod[prodCode[i]])["qtd"] = doc(this).text();
+      });
+      doc('vUnCom').each(function (i, element) {
+        (prod[prodCode[i]])["priceUnit"] = doc(this).text();
+      });
 
 
-            metadata = {
-                name: doc('xNome').text(),
-                fantasyName: doc('xFant').text(),
-                prod: prod,
-                date: date
-            };
+      metadata = {
+        name: doc('xNome').text(),
+        fantasyName: doc('xFant').text(),
+        prod: prod,
+        date: date
+      };
 
-            return saveList(uid, metadata, res);
+      return saveList(uid, metadata, res);
 
 
-        } else {
+    } else {
           //  console.log("request error");
-            return error;
+          return error;
         }
-    });    
+      });    
 }
 
 function saveList(uid, metadata, res) {
-    var listData = {
-        name: metadata.name,
-        prod: metadata.prod,
-        date: metadata.date
-    };
+  var listData = {
+    name: metadata.name,
+    prod: metadata.prod,
+    date: metadata.date
+  };
 
-    
-    return admin.database().ref('/users/' + uid + "/lists/" + metadata.fantasyName).set(listData).then(() => {
-        return admin.database().ref('/markets/' + metadata.fantasyName).update(listData);
+
+  return admin.database().ref('/users/' + uid + "/lists/" + metadata.fantasyName).set(listData).then(() => {
+    return admin.database().ref('/markets/' + metadata.fantasyName).update(listData);
         // TODO UPDATE PRODUCTS
-    }).then(() => {
+      }).then(() => {
         return res.status(200).send("OK");
-    });
-}
+      });
+    }
 //Teste Local
 //http://localhost:5000/anota-backend/us-central1/addList?uid=12345&link=http://nfce.sefaz.pe.gov.br/nfce-web/consultarNFCe?chNFe=26180421920821000116650050000111779051519177&nVersao=100&tpAmb=1&dhEmi=323031382D30342D32345431343A33343A31342D30333A3030&vNF=68.23&vICMS=3.17&digVal=&cIdToken=000001&cHashQRCode=BFFC6C762A27D77FF8C8B8FDB6B83C6296F6014F
 //http://localhost:5000/anota-backend/us-central1/addList?uid=12345&link=http://nfce.sefaz.pe.gov.br/nfce-web/consultarNFCe?chNFe=26180406057223027967650100000196741100418351&nVersao=100&tpAmb=1&dhEmi=323031382d30342d32395431303a32333a34322d30333a3030&vNF=663.96&vICMS=71.81&digVal=2f4e314952456f7149353159793352596972627664654e78574a413d&cIdToken=000001&cHashQRCode=3957073cfcd84f6ebb36718f179b3f65cf38f881
@@ -114,22 +114,22 @@ function saveList(uid, metadata, res) {
 
 
 exports.updateBestMarkets = functions.database.ref('/users/{pushId}/{market}')
-    .onCreate((snapshot, context) => 
-    {
-     
-      [list, price] = getList(snapshot);
-      console.log("The User: ",context.params.pushId, 
-            " in the Market: ", context.params.market, 
-            " Bought: ", list,
-            " For: R$", price);
+.onCreate((snapshot, context) => 
+{
 
-      getBestMarket(list, price, snapshot)
-      
-     
-      return true;
+  [list, price] = getList(snapshot);
+  console.log("The User: ",context.params.pushId, 
+    " in the Market: ", context.params.market, 
+    " Bought: ", list,
+    " For: R$", price);
+
+  getBestMarket(list, price, snapshot)
 
 
-  });
+  return true;
+
+
+});
 
 
 
@@ -137,8 +137,10 @@ getBestMarket = (list, price, snapshot) =>{
   var bestMarkets = []
   
   markets = database.ref('markets/').once('value').then(snap => {
+
     marketPrice = 0;  
     snap.forEach( market => {
+      
       marketPrice = 0;
       var haveAllProducts = true;
       var name = market.val().name
@@ -150,7 +152,7 @@ getBestMarket = (list, price, snapshot) =>{
           marketPrice += prodFullList[targetProduct.name].priceUnit*targetProduct.qtd;
         } 
         else{
-          marketPrice = -Infinity
+          marketPrice = -Infinity;
         }
       });
 
@@ -161,14 +163,19 @@ getBestMarket = (list, price, snapshot) =>{
           price: marketPrice
         });  
       }
-      return true;
     });
 
-    bestMarkets.sort((a, b)=>{
-      return a.price > b.price;
-    });
-    
-    return snapshot.ref.child('bestMarkets').set(bestMarkets);
+    if(bestMarkets)
+    {
+      
+      bestMarkets.sort((a, b)=>{
+        return a.price > b.price;
+      });
+
+      return snapshot.ref.child('bestMarkets').set(bestMarkets);  
+    }
+
+    return true;
   });
 
   return markets;
