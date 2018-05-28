b8175c30dfc396a5c01852241cc2e74079304c41
Lucas Cavalcanti <lucas.cavalcanti.santos@gmail.com>
2018-05-28 19:21:17 -0300

    update produtcs topic

--- functions/index.js
+++ functions/index.js
@@ -26,8 +26,8 @@ exports.addList = functions.https.onRequest((req, res) => {
     // Grab the text parameter.
     const uid = req.query.uid;
     const link = req.query.link;
-
-    requestList(link, uid, Date.now(), res);
+    var now = new Date();
+    requestList(link, uid, now.toJSON(), res);
 });
 
 function requestList(link, uid, date, res) {
@@ -38,7 +38,7 @@ function requestList(link, uid, date, res) {
             var prodName = [];
             var prod = {};
             doc('xProd').each(function (i, element) {
-                prodName[i] = doc(this).text();
+                prodName[i] = doc(this).text().replace(/\/|\&|\*|\%/g, " ");
                 prod[prodName[i]] = {
                     // name: doc(this).text()
                 };
@@ -79,15 +79,28 @@ function saveList(uid, metadata, res) {
         prod: metadata.prod,
         date: metadata.date
     };
-
-    var listProd = metadata.prod;
-    async.forEach(listProd, (i, element) => {
-        (i)["market"] = metadata.fantasyName;
-    });    
+    var listAtt = {
+        name: metadata.name,
+        date: metadata.date
+    };
+    
     return admin.database().ref('/users/' + uid + "/" + metadata.fantasyName).set(listData).then(() => {
-        return admin.database().ref('/markets/' + metadata.fantasyName).update(listData);    
+        admin.database().ref("/markets/" + metadata.fantasyName + "/prod/").update(listData.prod);
+        return admin.database().ref("/markets/" + metadata.fantasyName).update(listAtt);    
     }).then(() => {
-        return admin.database().ref('/products').update(listProd);
+        return admin.database().ref('/products/').once('value').then( (snapshot) => {
+            var listProd = metadata.prod;
+            async.forEach(Object.keys(metadata.prod), (i, element) => {
+                var markets = {};
+                if (snapshot.val()){
+                    if(snapshot.val()[i])
+                        markets = snapshot.val()[i]["markets"];
+                }
+                markets[metadata.fantasyName] = true;
+                listProd[i]["markets"] = markets;
+            });
+            admin.database().ref("/products/").update(listProd);
+        });        
     }).then(() => {
         return res.status(200).send("OK");
     });
@@ -95,23 +108,6 @@ function saveList(uid, metadata, res) {
 //Teste Local
 //http://localhost:5000/anota-backend/us-central1/addList?uid=12345&link=http://nfce.sefaz.pe.gov.br/nfce-web/consultarNFCe?chNFe=26180421920821000116650050000111779051519177&nVersao=100&tpAmb=1&dhEmi=323031382D30342D32345431343A33343A31342D30333A3030&vNF=68.23&vICMS=3.17&digVal=&cIdToken=000001&cHashQRCode=BFFC6C762A27D77FF8C8B8FDB6B83C6296F6014F
 //http://localhost:5000/anota-backend/us-central1/addList?uid=12345&link=http://nfce.sefaz.pe.gov.br/nfce-web/consultarNFCe?chNFe=26180406057223027967650100000196741100418351&nVersao=100&tpAmb=1&dhEmi=323031382d30342d32395431303a32333a34322d30333a3030&vNF=663.96&vICMS=71.81&digVal=2f4e314952456f7149353159793352596972627664654e78574a413d&cIdToken=000001&cHashQRCode=3957073cfcd84f6ebb36718f179b3f65cf38f881
-
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-// exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-//     .onCreate((snapshot, context) => {
-//         // Grab the current value of what was written to the Realtime Database.
-//         const original = snapshot.val();
-//         console.log('Uppercasing', context.params.pushId, original);
-//         const uppercase = original.toUpperCase();
-//         // You must return a Promise when performing asynchronous tasks inside a Functions such as
-//         // writing to the Firebase Realtime Database.
-//         // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-//         return snapshot.ref.parent.child('uppercase').set(uppercase);
-//     });
-
-//  
-
 
 
 
